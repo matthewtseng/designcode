d599cf916199416d1e7abf853b31da7cf013c141
Matthew Tseng <matthewwktseng@gmail.com>
2021-07-04 16:00:34 -0700

    initial commit

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300.0, height: 220.0)
+            .background(Color.blue)
+            .cornerRadius(20.0)
+            .shadow(radius: 20.0)
+            .offset(x: 0, y: -20.0)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                    
+                }
+                .padding()
+                
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20.0)
+            .shadow(radius: 20.0)
+        }
     }
 }
 
